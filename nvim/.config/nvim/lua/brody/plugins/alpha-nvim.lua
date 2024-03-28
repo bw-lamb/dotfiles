5289451ca8a32e60980ce1a2002b6627bec4b33f
Brody <brody.lamb30@gmail.com>
2024-03-28 09:09:54 -0300

    Addded nvim splash screen

---
+++ nvim/.config/nvim/lua/brody/plugins/alpha-nvim.lua
@@ -0,0 +1,31 @@
+return {
+    'goolord/alpha-nvim',
+	config = function ()
+		local alpha = require'alpha'
+        local dashboard = require'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+             [[                               __                ]],
+             [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+             [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+             [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+             [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+             [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+         }
+         dashboard.section.buttons.val = {
+        	dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+         	dashboard.button( "f", "🔍 Find File", ":Telescope find_files<CR>"),
+         	dashboard.button( "g", "🔍 Grep String", ":Telescope live_grep<CR>"),
+			dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
+         }
+         local handle = io.popen('fortune')
+         local fortune = handle:read("*a")
+         handle:close()
+         dashboard.section.footer.val = fortune
+
+         dashboard.config.opts.noautocmd = true
+
+         vim.cmd[[autocmd User AlphaReady echo 'ready']]
+
+         alpha.setup(dashboard.config)
+	end
+};
