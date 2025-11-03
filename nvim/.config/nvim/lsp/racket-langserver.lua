1a7122dd42ae0e9dc3fd0edc5828fb353e8a9302
Brody Lamb <brody.lamb30@gmail.com>
2025-11-03 14:04:34 -0400

    Add racket lsp server. Removed OCaml config

---
+++ nvim/.config/nvim/lsp/racket-langserver.lua
@@ -0,0 +1,5 @@
+return {
+	cmd = {'racket', '-l', 'racket-langserver'},
+	filetypes = {'racket'},
+	root_markers = {'.git'}
+}
--- nvim/.config/nvim/lua/brody/lsp.lua
+++ nvim/.config/nvim/lua/brody/lsp.lua
@@ -1,4 +1,4 @@
 vim.lsp.enable('luals')
 vim.lsp.enable('rust-analyzer')
 vim.lsp.enable('typescript-language-server')
-vim.lsp.enable('ocaml-lsp-server')
+vim.lsp.enable('racket-langserver')
