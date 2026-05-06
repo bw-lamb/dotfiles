a48a86c668b2eb71c21107d3a2f507f58512007c
Brody Lamb <brody.lamb30@gamil.com>
2026-05-06 19:35:05 -0300

    Add config for XMonad + other X11 utils

---
+++ xmonad/.config/xmonad/xmonad.hs
@@ -0,0 +1,55 @@
+import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.EZConfig
+import XMonad.Util.Loggers
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.SpawnOnce (spawnOnce)
+
+main :: IO ()
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . withEasySB (statusBarProp "xmobar" (pure theXmobarPP)) defToggleStrutsKey
+     $ theConfig 
+
+theConfig = def
+    { terminal = "alacritty"
+    , modMask = mod4Mask -- Mod key is Win key
+    , startupHook = theStartupHook
+    }
+
+theXmobarPP :: PP
+theXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
+theStartupHook :: X ()
+theStartupHook = do
+	         spawnOnce "feh --bg-fill --no-fehbg ~/.wallpapers/sotc.jpg"
+		 spawnOnce "xscreensaver -no-splash &"
